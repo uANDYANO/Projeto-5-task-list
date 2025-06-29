f93b94267d345c4c44e9e162317bebb6955aec56
Anderson Salviano <andyanodev@gmail.com>
2025-06-29 12:46:35 -0300

    criação da função de gerar novas tarefas junto da criação do layout da pagina de criação de tarefas

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -3,10 +3,9 @@ import { useDispatch } from 'react-redux'
 
 import * as S from './styles'
 
-import * as enums from '../../utils/enums/Tarefa'
-
 import { remover, editar } from '../../store/reducers/tarefas'
 import TarefaClass from '../../models/Tarefa'
+import { BotaoSalvar } from '../../styles'
 
 type Props = TarefaClass
 
@@ -49,7 +48,7 @@ const Tarefa = ({
       <S.BarraAcoes>
         {estaEditando ? (
           <>
-            <S.BotaoSalvar
+            <BotaoSalvar
               onClick={() => {
                 dispatch(
                   editar({
@@ -64,7 +63,7 @@ const Tarefa = ({
               }}
             >
               Salvar
-            </S.BotaoSalvar>
+            </BotaoSalvar>
             <S.BotaoCancelarRemover onClick={cancelarEdicao}>
               Cancelar
             </S.BotaoCancelarRemover>
